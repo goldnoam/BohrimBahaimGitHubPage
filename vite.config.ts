beca393275f77e42d5b79b296a519396f09f9b09
Noam Gold <37283407+goldnoam@users.noreply.github.com>
2025-12-24 22:18:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BohrimBahaim/', // This is the crucial part!
+  base: '/BohrimBahaim/', // This must match your repo name!
   plugins: [react()],
 })
